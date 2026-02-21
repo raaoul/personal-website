ac9b18b9934a90a919f0197183b507a26c3945fb
Raoulye Agustino <raoulyea@gmail.com>
2026-02-21 15:17:06 +0700

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { ModeToggle } from "@/components/mode-toggle"
+
+export default function Page() {
+  return (
+    <main className="container mx-auto my-16">
+      <div className="mb-4 flex justify-end">
+        <ModeToggle />
+      </div>
+      <p>PAGES!!!</p>
+    </main>
+  )
+}
